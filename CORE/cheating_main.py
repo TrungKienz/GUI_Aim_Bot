38cfc877974bb2fbd41eb4a1ea157607caec7496
buitrungkien <buitrungkien@vnpt.vn>
2025-04-06 16:02:37 +0700

    update: 04/06

--- CORE/cheating_main.py
+++ CORE/cheating_main.py
@@ -9,8 +9,7 @@ FOV = 100
 CENTER_X, CENTER_Y = 1920 // 2, 1080 // 2
 MOUSE_SENSITIVITY = 0.2
 
-def cheating_main(toggle_key=TOGGLE_KEY, fov=FOV, center_x=CENTER_X, center_y=CENTER_Y, mouse_sensitivity=MOUSE_SENSITIVITY, border_color='purple', hold_key=0x06):
-    os.system('title Valorant ANT BOT')
+def cheating_main(toggle_key=TOGGLE_KEY, fov=FOV, center_x=CENTER_X, center_y=CENTER_Y, mouse_sensitivity=MOUSE_SENSITIVITY, border_color=' ', hold_key=0x06, log_func=print):
     colorant = Colorant(center_x - FOV // 2, center_y - fov // 2, fov, mouse_sensitivity, border_color, hold_key)
     status = 'Disabled'
     
@@ -19,7 +18,9 @@ def cheating_main(toggle_key=TOGGLE_KEY, fov=FOV, center_x=CENTER_X, center_y=CE
             if keyboard.is_pressed(toggle_key):
                 colorant.toggle()
                 status = 'Enabled ' if colorant.toggled else 'Disabled'
-            print(f'\r{colored("[Status]", "green")} {colored(status, "white")}', end='')
+
+            log_func(f"[Status] {status}")
+            log_func(f'\r{colored("[Status]", "green")} {colored(status, "white")}', end='')
             time.sleep(0.01)
     except (KeyboardInterrupt, SystemExit):
         print(colored('\n[Info]', 'green'), colored('Exiting...', 'white') + '\n')
--- GUI/main_app.py
+++ GUI/main_app.py
@@ -1,17 +1,23 @@
+import threading
 import wx
 
 from CORE.cheating_main import cheating_main
 
 class MainAppFrame(wx.Frame):
     def __init__(self):
-        super().__init__(None, title="Main App", size=(400, 450))
+        super().__init__(None, title="Main App", size=(400, 600), style=wx.DEFAULT_FRAME_STYLE & ~(wx.RESIZE_BORDER | wx.MAXIMIZE_BOX))
         panel = wx.Panel(self)
         vbox = wx.BoxSizer(wx.VERTICAL)
 
         sensitivity_label = wx.StaticText(panel, label="Độ nhạy:")
-        self.sensitivity_input = wx.TextCtrl(panel)
+        self.sensitivity_input = wx.TextCtrl(panel, value="0.2")
         vbox.Add(sensitivity_label, flag=wx.LEFT | wx.TOP, border=10)
         vbox.Add(self.sensitivity_input, flag=wx.EXPAND | wx.LEFT | wx.RIGHT, border=10)
+
+        pov_label = wx.StaticText(panel, label="Điều chỉnh khoảng nhận diện:")
+        self.fov_input = wx.TextCtrl(panel, value="100")
+        vbox.Add(pov_label, flag=wx.LEFT | wx.TOP, border=10)
+        vbox.Add(self.fov_input, flag=wx.EXPAND | wx.LEFT | wx.RIGHT, border=10)
 
         active_program_label = wx.StaticText(panel, label="Tùy chọn gán phím để bật chương trình:")
         vbox.Add(active_program_label, flag=wx.LEFT | wx.TOP, border=10)
@@ -22,6 +28,26 @@ class MainAppFrame(wx.Frame):
         vbox.Add(self.radio_mouse4, flag=wx.LEFT | wx.TOP, border=10)
         vbox.Add(self.radio_mouse5, flag=wx.LEFT | wx.TOP, border=10)
         vbox.Add(self.radio_alt, flag=wx.LEFT | wx.TOP, border=10)
+
+        resolution_label = wx.StaticText(panel, label="Chọn độ phân giải của màn hình:")
+        vbox.Add(resolution_label, flag=wx.LEFT | wx.TOP, border=10)
+        self.radio_fhd = wx.RadioButton(panel, label="Màn hình Full HD (1920x1080)", style = wx.RB_GROUP)
+        self.radio_2k = wx.RadioButton(panel, label="Màn hình 2K (2560x1440)")
+        self.radio_4k = wx.RadioButton(panel, label="Màn hình 4K (3840x2160)")
+
+        vbox.Add(self.radio_fhd, flag=wx.LEFT | wx.TOP, border=10)
+        vbox.Add(self.radio_2k, flag=wx.LEFT | wx.TOP, border=10)
+        vbox.Add(self.radio_4k, flag=wx.LEFT | wx.TOP, border=10)
+
+        character_outline_label = wx.StaticText(panel, label="Chọn đường viền nhân vật:")
+        vbox.Add(character_outline_label, flag=wx.LEFT | wx.TOP, border=10)
+        self.radio_purple = wx.RadioButton(panel, label="Đường viền tím", style = wx.RB_GROUP)
+        self.radio_red = wx.RadioButton(panel, label="Đường viền đỏ")
+        self.radio_yellow = wx.RadioButton(panel, label="Đường viền vàng")
+
+        vbox.Add(self.radio_purple, flag=wx.LEFT | wx.TOP, border=10)
+        vbox.Add(self.radio_red, flag=wx.LEFT | wx.TOP, border=10)
+        vbox.Add(self.radio_yellow, flag=wx.LEFT | wx.TOP, border=10)
 
 
         self.key_button = wx.Button(panel, label="Start")
@@ -38,17 +64,44 @@ class MainAppFrame(wx.Frame):
         self.Show()
 
     def log(self, message):
-        self.terminal_output.AppendText(message + "\n")
+        # self.terminal_output.AppendText(message + "\n")
+        wx.CallAfter(self.terminal_output.AppendText, message + "\n")
 
     def start_cheating(self, event):
         if self.radio_mouse4.GetValue():
             hold_key = '0x05'
-            self.log(self.sensitivity_input.GetValue())
         elif self.radio_mouse5.GetValue():
             hold_key = '0x06'
         elif self.radio_alt.GetValue():
             hold_key = '0xA4'
+        
+        if self.radio_fhd.GetValue():
+            center_x, center_y = 1920 // 2, 1080 // 2
+        elif self.radio_2k.GetValue():
+            center_x, center_y = 2560 // 2, 1440 // 2
+        elif self.radio_4k.GetValue():
+            center_x, center_y = 3840 // 2, 2160 // 2
 
-        
-        
-        cheating_main(hold_key=hold_key)
+        if self.radio_purple.GetValue():
+            border_color = 'purple'
+        elif self.radio_red.GetValue():
+            border_color = 'red'
+        elif self.radio_yellow.GetValue(): 
+            border_color = 'yellow'
+
+        try:
+            fov = int(self.fov_input.GetValue())
+            sensitivity = float(self.sensitivity_input.GetValue())
+            self.log("Độ nhạy chuột: " + str(sensitivity))
+        except ValueError:
+            self.log("Giá trị độ nhạy không hợp lệ.")
+            return
+
+        def run_cheating():
+            try:
+                cheating_main(fov=fov,center_x=center_x, center_y=center_y, mouse_sensitivity=sensitivity, border_color=border_color, hold_key=hold_key, log_func=self.log)
+                wx.CallAfter(self.log, "Chương trình đã bắt đầu.") 
+            except Exception as e:
+                wx.CallAfter(self.log, f"Lỗi: {e}")
+
+        threading.Thread(target=run_cheating, daemon=True).start()
